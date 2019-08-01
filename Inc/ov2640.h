e4d70eb452f8529a33ad80b2aadaf991cdaafee8
Duy Anh Pham <pham@ad-hoc.com>
2019-08-01 15:11:13 +0200

    remove unnecessary file in ov2640 driver

--- Inc/ov2640.h
+++ Inc/ov2640.h
@@ -203,16 +203,6 @@ void     CAMERA_IO_Write(uint8_t addr, uint8_t reg, uint8_t value);
 uint8_t  CAMERA_IO_Read(uint8_t addr, uint8_t reg);
 void     CAMERA_Delay(uint32_t delay);
 
-void I2Cx_MspInit(void);
-void I2Cx_Init();
-void I2Cx_ITConfig(void);
-void I2Cx_Write(uint8_t Addr, uint8_t Reg, uint8_t Value);
-uint8_t I2Cx_Read(uint8_t Addr, uint8_t Reg);
-HAL_StatusTypeDef I2Cx_ReadMultiple(uint8_t Addr, uint16_t Reg, uint16_t MemAddSize, uint8_t *Buffer, uint16_t Length);
-HAL_StatusTypeDef I2Cx_WriteMultiple(uint8_t Addr, uint16_t Reg, uint16_t MemAddSize, uint8_t *Buffer, uint16_t Length);
-HAL_StatusTypeDef I2Cx_IsDeviceReady(uint16_t DevAddress, uint32_t Trials);
-void I2Cx_Error(uint8_t Addr);
-
 
 /* CAMERA driver structure */
 extern CAMERA_DrvTypeDef   ov2640_drv;
